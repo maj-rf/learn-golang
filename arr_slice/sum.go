d8f09abe3e843f23f8aa9bcd35c65cfedc57354e
maj-rf <rmkfermin@gmail.com>
2024-10-02 16:25:21 +0800

    feat: finish iteration, add sumAllTails and related tests

--- arr_slice/sum.go
+++ arr_slice/sum.go
@@ -15,3 +15,32 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+/*
+	s := []int{2, 3, 5, 7, 11, 13}
+	Slice the slice to give it zero length.
+	s = s[:0]
+	Output: len=0 cap=6 []
+
+	Extend its length.
+	s = s[:4]
+	Output: len=4 cap=6 [2 3 5 7]
+
+	Drop its first two values.
+	s = s[2:]
+	Output: len=2 cap=4 [5 7]
+*/
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+
+	}
+	return sums
+}
--- arr_slice/sum_test.go
+++ arr_slice/sum_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+/*
+	go test -v
+
+	to check 100% coverage
+	go test -cover
+*/
+
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
@@ -28,8 +35,27 @@ func TestSum(t *testing.T) {
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
-
 	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("get the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("get the sums even with empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
 }
