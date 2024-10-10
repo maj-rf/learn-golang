9e29834e954bf29033091874b84745df32b0e9a2
maj-rf <rmkfermin@gmail.com>
2024-10-10 09:58:29 +0800

    feat: finish concurrency -use goroutines and channels to control concurrent operations and avoid race conditions

--- 09_concurrency/check_websites.go
+++ 09_concurrency/check_websites.go
@@ -1,11 +1,35 @@
 package concurrency
 
+/*
+- Concurrency: "having more than one thing in progress"
+- If we wait for a function to return something, it's a "blocking" operation
+- goroutine: an operation that doesn't block in go and will run in a
+  separate process
+- go test -race : spot race conditions with built-in race detector
+- channels, to help organize and control the communication between the different
+  processes, allowing us to avoid a race condition bug.
+- resultChannel <- result{u, wc(u)} // send statement
+- r := <-resultChannel // receive expression
+*/
+
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 	return results
 }
